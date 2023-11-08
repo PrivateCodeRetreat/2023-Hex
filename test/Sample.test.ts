71aade15a7fe6da73ee195f3d1fbb831593d951b
Lada Kesseler <lada.kesseler1@t-mobile.com>
2023-11-08 11:58:33 -0800

    ! F printing hex board

--- test/Sample.test.ts
+++ test/Sample.test.ts
@@ -1,49 +1,31 @@
-import {beforeAll, describe, expect, test} from "@jest/globals";
-import {configure} from "approvals";
-import {JestReporter} from "approvals/lib/Providers/Jest/JestReporter";
-import {verify, verifyAsJson} from "approvals/lib/Providers/Jest/JestApprovals";
+import {describe, test} from "@jest/globals";
+import {verify} from "approvals/lib/Providers/Jest/JestApprovals";
 
 class GameOfLifeBoard {
 
     printHexBoard(width: number, height: number): string {
-            // The pieces that make up each hex cell
-            const flatTop = ' __ ';
-            const topLeft = '/  \\';
-            const bottomLeft = '\\__/';
+        // The pieces that make up each hex cell
+        const top = "\\__/  ";
+        const bottom = "/  \\__";
 
-            // The output grid string
-            let grid = '';
+        let grid = '';
 
-            // Create the top part of the grid
-            for (let row = 0; row < height; row++) {
-                let topLine = '';
-                let bottomLine = '';
-
-                // Create the offset for the grid
-                if (row % 2 !== 0) {
-                    topLine += ' ';
-                    bottomLine += ' ';
-                }
-
-                // Create each cell in the row
-                for (let col = 0; col < width; col++) {
-                    topLine += flatTop;
-                    bottomLine += topLeft + bottomLeft;
-                }
-
-                // Remove the last bottomLeft from the bottomLine string
-                bottomLine = bottomLine.substring(0, bottomLine.length - bottomLeft.length);
-
-                // Add the completed lines to the grid
-                grid += topLine + '\n' + bottomLine + '\n';
-            }
-
-            // Remove the last newline character from the grid string
-            grid = grid.substring(0, grid.length - 1);
-
-            return grid;
+        grid = this.printColumn(width, grid, " __   ");
+        for (let row = 0; row < Math.ceil(height / 2); row++) {
+            grid = this.printColumn(width, grid, bottom);
+            grid = this.printColumn(width, grid, top);
         }
 
+        return grid;
+    }
+
+    private printColumn(width: number, grid: string, pattern: string) {
+        for (let col = 0; col < width; col++) {
+            grid += pattern;
+        }
+
+        return grid.substring(0, grid.length - 2) + "\n";
+    }
 
     toString() {
         return this.printHexBoard(2, 3);
